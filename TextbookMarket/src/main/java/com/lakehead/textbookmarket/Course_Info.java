f24d0c1230cfb1f353c420a41c0883feed260441
mikecolistro <mike_colistro@hotmail.com>
2014-04-03 00:34:02 -0400

    Added Dummy data and list for courses books
    and more awesome gradle changes, WOOT

--- TextbookMarket/src/main/java/com/lakehead/textbookmarket/Course_Info.java
+++ TextbookMarket/src/main/java/com/lakehead/textbookmarket/Course_Info.java
@@ -1,21 +1,77 @@
 package com.lakehead.textbookmarket;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class Course_Info extends Activity {
-
+    Button bookButton;
+    private AlertDialog alertDialogStores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course__info);
+        bookButton = (Button) findViewById(R.id.bookButton);
+        bookButton.setBackgroundResource(R.drawable.showbooksbutton);
+
+
+        ObjectItem[] ObjectItemData = new ObjectItem[20];
+
+        ObjectItemData[0] = new ObjectItem(91, "Mercury");
+        ObjectItemData[1] = new ObjectItem(92, "Watson");
+        ObjectItemData[2] = new ObjectItem(93, "Nissan");
+        ObjectItemData[3] = new ObjectItem(94, "Puregold");
+        ObjectItemData[4] = new ObjectItem(95, "SM");
+        ObjectItemData[5] = new ObjectItem(96, "7 Eleven");
+        ObjectItemData[6] = new ObjectItem(97, "Ministop");
+        ObjectItemData[7] = new ObjectItem(98, "Fat Chicken");
+        ObjectItemData[8] = new ObjectItem(99, "Master Siomai");
+        ObjectItemData[9] = new ObjectItem(100, "Mang Inasal");
+        ObjectItemData[10] = new ObjectItem(101, "Mercury 2");
+        ObjectItemData[11] = new ObjectItem(102, "Watson 2");
+        ObjectItemData[12] = new ObjectItem(103, "Nissan 2");
+        ObjectItemData[13] = new ObjectItem(104, "Puregold 2");
+        ObjectItemData[14] = new ObjectItem(105, "SM 2");
+        ObjectItemData[15] = new ObjectItem(106, "7 Eleven 2");
+        ObjectItemData[16] = new ObjectItem(107, "Ministop 2");
+        ObjectItemData[17] = new ObjectItem(108, "Fat Chicken 2");
+        ObjectItemData[18] = new ObjectItem(109, "Master Siomai 2");
+        ObjectItemData[19] = new ObjectItem(110, "Mang Inasal 2");
+
+        // our adapter instance
+        SpecificListingArrayAdapter adapter = new SpecificListingArrayAdapter(Course_Info.this, ObjectItemData);
+
+        // create a new ListView, set the adapter and item click listener
+        final ListView listViewItems = new ListView(this);
+        listViewItems.setAdapter(adapter);
+        listViewItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+        });
+
+        bookButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                    Log.d("debug","There is a listing for the book and we would go to the listing here");
+                    alertDialogStores = new AlertDialog.Builder(Course_Info.this).setView(listViewItems).setTitle("Stores").show();
+
+            }
+        });
 
         Intent intent = getIntent();
         Course thisCourse = intent.getParcelableExtra("course");
